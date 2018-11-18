253e907a9239e9732e7e889e8cd42a04c3f5d596
DownToSky <mme94@scarletmail.rutgers.edu>
2018-11-18 16:31:32 -0500

    tileset done :o

--- Godot/KinematicBody2D.gd
+++ Godot/KinematicBody2D.gd
@@ -1,43 +1,25 @@
 extends KinematicBody2D
 
-var motion = Vector2(0, 0)
 
-var MAX_SPEED_X = 100
-var MAX_SPEED_Y = 100
 
-func filter_speed():
-	if motion.x > MAX_SPEED_X:
-		motion.x = MAX_SPEED_X
-	elif motion.x < -MAX_SPEED_X:
-		motion.x = -MAX_SPEED_X
-	if motion.y > MAX_SPEED_Y:
-		motion.y = MAX_SPEED_Y
-	elif motion.y < -MAX_SPEED_Y:
-		motion.y = -MAX_SPEED_Y
+export(int) var SPEED = 40
+export(float) var DASH_MULTIPLIER = 3
 
 func _physics_process(delta):
-	if Input.is_action_just_released('ui_right'):
-		motion.x = 0
-	if Input.is_action_just_released('ui_left'):
-		motion.x = 0
-	if Input.is_action_just_released('ui_up'):
-		motion.y = 0
-	if Input.is_action_just_released('ui_down'):
-		motion.y = 0
-	
+	var motion = Vector2(0, 0)
+	var speed = SPEED
+	if Input.is_action_pressed('ui_dash'):
+		speed =  DASH_MULTIPLIER * SPEED
 	if Input.is_action_pressed('ui_right'):
-		motion.x += 100
-		filter_speed()
+		motion.x += 1
 	if Input.is_action_pressed('ui_left'):
-		motion.x += -100
-		filter_speed()
+		motion.x += -1
 	if Input.is_action_pressed('ui_up'):
-		motion.y += -100
-		filter_speed()
+		motion.y += -1
 	if Input.is_action_pressed('ui_down'):
-		motion.y += 100
-		filter_speed()
+		motion.y += 1
 	
 	
-	move_and_slide(motion)
+	
+	move_and_slide(motion.normalized() * speed, Vector2(0, -1))
 	
