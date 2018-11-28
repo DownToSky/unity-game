0c835aae4616026ff914942ff2e7a28e41e9baf1
DownToSky <mme94@scarletmail.rutgers.edu>
2018-11-28 09:32:12 -0500

    8 point direction to mouse direction

--- Godot/Scripts/KinematicBody2D.gd
+++ Godot/Scripts/KinematicBody2D.gd
@@ -1,15 +1,36 @@
 extends KinematicBody2D
 
 
-export(int) var SPEED = 100 * 60
+export(int) var SPEED = 50 * 60
 export(float) var DASH_MULTIPLIER = 3
 
 var direction = Vector2(0,1)
 onready var ANIM_SPEED = $AnimatedSprite.get_sprite_frames().get_animation_speed($AnimatedSprite.get_animation())
 
+
+func many2eight_direction():
+	$Camera2D/text1.text = String(Vector2(1,0).angle_to(Vector2(-1,0)))
+	var possible_dirs = Array()
+	for i in range(-1,2):
+		for j in range(-1,2):
+			possible_dirs.append(Vector2(i,j))
+	var least_diff = null
+	var least_dir = null
+	for dir in possible_dirs:
+		var new_diff = 360 + rad2deg(dir.angle_to(self.get_local_mouse_position()))
+		if least_diff == null or least_diff >  new_diff:
+			least_diff = new_diff
+			least_dir = dir
+	least_dir.x *= -1 
+	least_dir.y *= -1 		
+	return least_dir
+
+
 func _ready():
 	set_physics_process(true)
 	
+
+
 func _physics_process(delta):
 	var motion = Vector2(0, 0)
 
@@ -22,58 +43,60 @@ func _physics_process(delta):
 	if Input.is_action_pressed('ui_down'):
 		motion.y += 1
 	
-	if motion != Vector2(0,0):
-		direction = motion
-		var angle = rad2deg(motion.angle()) - 90
-		$Light2D.set_rotation_degrees(angle)
+	var dir = many2eight_direction()
 	
+	$Flashlight.look_at(get_global_mouse_position())
+	$Flashlight.rotate(deg2rad(-90))
+	
+	$Camera2D/text2.text = "motion on"
 	if motion == Vector2(0,0):
-		if direction == Vector2(0,1):
+		$Camera2D/text2.text = "motion off"
+		if dir == Vector2(0,1):
 			$AnimatedSprite.flip_h = false
 			$AnimatedSprite.play("idle_down")
-		elif direction == Vector2(1,1):
+		elif dir == Vector2(1,1):
 			$AnimatedSprite.flip_h = false
 			$AnimatedSprite.play("idle_down_right")
-		elif direction == Vector2(1,0):
+		elif dir == Vector2(1,0):
 			$AnimatedSprite.flip_h = false
 			$AnimatedSprite.play("idle_right")
-		elif direction == Vector2(1,-1):
+		elif dir == Vector2(1,-1):
 			$AnimatedSprite.flip_h = false
 			$AnimatedSprite.play("idle_up_right")
-		elif direction == Vector2(0,-1):
+		elif dir == Vector2(0,-1):
 			$AnimatedSprite.flip_h = false
 			$AnimatedSprite.play("idle_up")
-		elif direction == Vector2(-1,-1):
+		elif dir == Vector2(-1,-1):
 			$AnimatedSprite.flip_h = true
 			$AnimatedSprite.play("idle_up_right")
-		elif direction == Vector2(-1,0):
+		elif dir == Vector2(-1,0):
 			$AnimatedSprite.flip_h = true
 			$AnimatedSprite.play("idle_right")
-		elif direction == Vector2(-1,1):
+		elif dir == Vector2(-1,1):
 			$AnimatedSprite.flip_h = true
 			$AnimatedSprite.play("idle_right")
-	elif motion == Vector2(0,1):
+	elif dir == Vector2(0,1):
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("walk_down")
-	elif motion == Vector2(1,1):
+	elif dir == Vector2(1,1):
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("walk_down_right")
-	elif motion == Vector2(1,0):
+	elif dir == Vector2(1,0):
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("walk_right")
-	elif motion == Vector2(1,-1):
+	elif dir == Vector2(1,-1):
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("walk_up_right")
-	elif motion == Vector2(0,-1):
+	elif dir == Vector2(0,-1):
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("walk_up")
-	elif motion == Vector2(-1,-1):
+	elif dir == Vector2(-1,-1):
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("walk_up_right")
-	elif motion == Vector2(-1,0):
+	elif dir == Vector2(-1,0):
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("walk_right")
-	elif motion == Vector2(-1,1):
+	elif dir == Vector2(-1,1):
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("walk_down_right")
 	
